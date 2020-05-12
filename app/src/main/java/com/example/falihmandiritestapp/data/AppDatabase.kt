3d7ea28653e1f3053aadc9ee65400788edc84e35
Falih Mulyana <falihakz@gmail.com>
2020-05-12 22:09:26 +0700

    Adding converter for entity

--- app/src/main/java/com/example/falihmandiritestapp/data/AppDatabase.kt
+++ app/src/main/java/com/example/falihmandiritestapp/data/AppDatabase.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.falihmandiritestapp.BuildConfig
 import com.example.falihmandiritestapp.data.dao.ArticleDao
 import com.example.falihmandiritestapp.data.entity.Article
 import java.io.File
 
 @Database(entities = [Article::class], version = 1)
+@TypeConverters(EntityFieldConverters::class)
 abstract class AppDatabase :RoomDatabase() {
 
     abstract fun articleDao(): ArticleDao
